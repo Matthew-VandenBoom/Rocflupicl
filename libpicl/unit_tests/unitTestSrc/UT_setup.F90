c5d04e0c53ed852cd31151c3a6be1df6f669ce4c
Matthew VandenBoom <MatthewVandenBoom@outlook.com>
2026-03-16 14:51:33 -0400

    Renamed unit test files for F90

--- libpicl/unit_tests/unitTestSrc/UT_setup.F90
+++ libpicl/unit_tests/unitTestSrc/UT_setup.F90
@@ -1,5 +1,4 @@
-#include"../../ppiclF/source/PPICLF_USER.h"
-#include"../../ppiclF/source/PPICLF_STD.h"
+#include "PPICLF_STD.h"
 
 !**********************************************************************
 ! This setup_Unit Test should be called at the beginning of each unit
